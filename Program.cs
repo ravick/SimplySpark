d84a03a3a665b23a205ee6248cf5f5fc00a2a70e
Ravi C Kolandaiswamy <ravick@cloudassert.com>
2021-10-08 11:54:44 -0700

    gitignore and SparkTest

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.Spark.Sql;
+using static Microsoft.Spark.Sql.Functions;
+using System;
+using System.Collections.Generic;
+using Microsoft.Spark.Sql.Types;
 
 namespace SimplySpark
 {
@@ -6,7 +10,42 @@ namespace SimplySpark
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            SparkTest();
+        }
+         static void SparkTest()
+        {
+            Console.WriteLine($"Running simple Spark Test: Create and show sample data frame.");
+            var data = new List<GenericRow>
+                        {
+                            new GenericRow(new object[] { 1, "John Doe" }),
+                            new GenericRow(new object[] { 2, "Jane Doe" }),
+                            new GenericRow(new object[] { 3, "Foo Bar" })
+                        };
+
+            var schema = new Microsoft.Spark.Sql.Types.StructType(new List<StructField>()
+                        {
+                            new StructField("Id", new IntegerType()),
+                            new StructField("Name", new StringType()),
+                        });
+
+            SparkSession spark = SparkSession
+               .Builder()
+               .AppName("Coconut Spark Test")
+               .GetOrCreate();
+            
+            Console.WriteLine("Spark Configuration");
+            
+            foreach (var conf in spark.SparkContext.GetConf().GetAll())
+            {
+                Console.WriteLine($"Conf: {conf.Key} = {conf.Value}");
+            }
+            
+            DataFrame df = spark.CreateDataFrame(data, schema);
+            df.Show();
+            Console.WriteLine($"If you see a tabular output with names, then the Spark Test Successful !!!");
+            Console.WriteLine("Press enter to complete..");
+            Console.ReadLine();
+            spark.Stop();
         }
     }
 }
